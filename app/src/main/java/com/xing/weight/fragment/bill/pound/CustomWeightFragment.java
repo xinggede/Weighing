8038d1c3b9c51793b3cb009aafd989c24ce2b9a3
xinggede <445871128@qq.com>
2020-12-10 13:49:52 +0800

    调整

---
+++ app/src/main/java/com/xing/weight/fragment/bill/pound/CustomWeightFragment.java
@@ -0,0 +1,41 @@
+package com.xing.weight.fragment.bill.pound;
+
+import android.view.View;
+
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+import com.xing.weight.R;
+import com.xing.weight.base.BaseFragment;
+import com.xing.weight.fragment.main.MainContract;
+import com.xing.weight.fragment.main.MainPresenter;
+
+import butterknife.BindView;
+
+public class CustomWeightFragment extends BaseFragment<MainPresenter> implements MainContract.View {
+
+
+    @BindView(R.id.topbar)
+    QMUITopBarLayout topbar;
+
+    @Override
+    protected MainPresenter onLoadPresenter() {
+        return new MainPresenter();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_custom_weight;
+    }
+
+    @Override
+    protected void initView(View view) {
+        topbar.setTitle("磅单定制");
+        topbar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popBackStack();
+            }
+        });
+    }
+
+
+}
